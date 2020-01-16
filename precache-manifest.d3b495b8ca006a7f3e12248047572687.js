71df6dd0b90582651d14f3540aefb4f936862fa9
Kamran Nawaz <kammy007@hotmail.com>
2020-01-16 15:55:54 +0000

    Updates

---
+++ precache-manifest.d3b495b8ca006a7f3e12248047572687.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7929044c28d8d6d37567affd7197bf",
+    "url": "/digitalcertificate/index.html"
+  },
+  {
+    "revision": "9ebfd35918dc6c82650e",
+    "url": "/digitalcertificate/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "339221dbbcbe9641f882",
+    "url": "/digitalcertificate/static/css/main.47cdbd07.chunk.css"
+  },
+  {
+    "revision": "9ebfd35918dc6c82650e",
+    "url": "/digitalcertificate/static/js/2.7843739e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/digitalcertificate/static/js/2.7843739e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "339221dbbcbe9641f882",
+    "url": "/digitalcertificate/static/js/main.a043bcc6.chunk.js"
+  },
+  {
+    "revision": "722c91c721fa222487cf",
+    "url": "/digitalcertificate/static/js/runtime-main.4b8d7974.js"
+  },
+  {
+    "revision": "ddf46f6911a51aef0362582c68ec0ced",
+    "url": "/digitalcertificate/static/media/faceimage.ddf46f69.png"
+  }
+]);
